e9403e72312227e001b37ee75b11b4eaef0d89b7
Bruno Nascimento da Costa <brunobass@live.com>
2025-12-24 00:57:44 -0300

    Linha de código duplicado, correção para melhor funcionamento

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Cadastro.css";
+import "../Cadastro/Cadastro.css";
 import { useNavigate } from "react-router-dom";
 
 export default function Cadastro() {
@@ -48,6 +48,7 @@ export default function Cadastro() {
         body: JSON.stringify({
           nome_completo: form.nome_completo,
           email: form.email,
+          telefone: form.telefone,
           escola: form.escola,
           tipo_usuario: form.tipo_usuario,
           senha: form.senha,
@@ -80,18 +81,14 @@ export default function Cadastro() {
         <div className="cadastro-subtitle">
           Comece sua jornada de desenvolvimento agora
         </div>
-        <div className="cadastro-subtitle">
-          Comece sua jornada de desenvolvimento agora
-        </div>
 
-        <form onSubmit={handleCriarConta}>
         <form onSubmit={handleCriarConta}>
           <div className="input-group">
             <label>Nome completo</label>
             <input
               type="text"
-              name="nome"
-              value={form.nome}
+              name="nome_completo"
+              value={form.nome_completo}
               onChange={handleChange}
             />
           </div>
@@ -125,20 +122,14 @@ export default function Cadastro() {
               value={form.escola}
               onChange={handleChange}
             />
-            <input
-              type="text"
-              name="escola"
-              value={form.escola}
-              onChange={handleChange}
-            />
           </div>
 
           <div className="input-group">
             <label>Tipo de usuário</label>
             <div className="select-wrapper">
               <select
-                name="tipoUsuario"
-                value={form.tipoUsuario}
+                name="tipo_usuario"
+                value={form.tipo_usuario}
                 onChange={handleChange}
               >
                 <option value="">Selecione</option>
@@ -156,12 +147,6 @@ export default function Cadastro() {
               value={form.senha}
               onChange={handleChange}
             />
-            <input
-              type="password"
-              name="senha"
-              value={form.senha}
-              onChange={handleChange}
-            />
           </div>
 
           <div className="input-group">
@@ -172,15 +157,8 @@ export default function Cadastro() {
               value={form.confirmarSenha}
               onChange={handleChange}
             />
-            <input
-              type="password"
-              name="confirmarSenha"
-              value={form.confirmarSenha}
-              onChange={handleChange}
-            />
           </div>
 
-          <button type="submit" className="btn-criar-conta">
           <button type="submit" className="btn-criar-conta">
             Criar Conta
           </button>
